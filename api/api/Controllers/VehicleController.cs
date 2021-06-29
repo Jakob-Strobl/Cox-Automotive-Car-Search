3191ce6bda23d825707fac4708f25cf7ca69a2b9
jakob <Jakobpstrobl@gmail.com>
2021-06-29 17:23:27 -0400

    Setup .net API with static Vehicle get() request

---
+++ api/api/Controllers/VehicleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VehicleController : ControllerBase
+    {
+        [HttpGet]
+        public Vechicle Get()
+        {
+            return new Vechicle
+            {
+                GUID = new Guid("c417a7a3-4508-4e7d-9189-d8df6641dd71"),
+                Make = new string("Lincoln"),
+                Model = new string("MKT"),
+                Year = 2011,
+                Vin = new string("1B3AZ6JZ7AV721867"),
+                Trim = new string("LSX"),
+            };
+        }
+    }
+}
---
+++ api/api/Vechicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api
+{
+    public class Vechicle
+    {
+        // "id" field from json
+        public Guid GUID { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string Vin { get; set; }
+
+        public string Trim { get; set; }
+    }
+}
